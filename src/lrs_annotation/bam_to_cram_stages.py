112557de9fa354b195f2399571b5018f98801d07
EddieLF <eddieformaini@gmail.com>
2025-07-16 11:54:18 +1000

    Add BamToCram stage and job

---
+++ src/lrs_annotation/bam_to_cram_stages.py
@@ -0,0 +1,58 @@
+"""
+Workflow for annotating long-read SVs data into a seqr-ready format.
+"""
+
+from cpg_utils import Path
+from cpg_utils.config import config_retrieve, reference_path
+from cpg_utils.hail_batch import get_batch
+
+from cpg_flow import stage, targets
+from cpg_flow.filetypes import CramPath
+
+from jobs.BamToCram import bam_to_cram
+
+
+def make_long_read_cram_path(sg: targets.SequencingGroup) -> CramPath:
+    """
+    Path to a CRAM file. Not checking its existence here.
+    """
+    path: Path = sg.dataset.prefix() / 'long_read' / 'cram' / f'{sg.id}.cram'
+    return CramPath(
+        path=path,
+        index_path=path.with_suffix('.cram.crai'),
+        reference_assembly=config_retrieve(['workflow', 'ref_fasta'], reference_path('broad/ref_fasta')),
+    )
+
+
+@stage.stage(
+    analysis_type=config_retrieve(['workflow', 'bam_to_cram', 'analysis_type'], 'cram'),
+    analysis_keys=['cram'],
+)
+class ConvertPacBioBamToCram(stage.SequencingGroupStage):
+    """
+    Convert a PacBio BAM to a CRAM file.
+    """
+
+    def expected_outputs(self, sg: targets.SequencingGroup) -> dict[str, Path]:
+        """
+        Stage is expected to generate a CRAM file and a corresponding index.
+        """
+        cram_path = sg.cram or make_long_read_cram_path(sg)
+        return {'cram': cram_path.path, 'crai': cram_path.index_path}
+
+    def queue_jobs(self, sg: targets.SequencingGroup, inputs: stage.StageInput) -> stage.StageOutput | None:
+        """
+        Using the existing `bam_to_cram` function from the `jobs` module.
+        """
+        b = get_batch()
+
+        input_bam = b.read_input_group(bam=str(sg.alignment_input))
+        job = bam_to_cram(
+            b=get_batch(),
+            input_bam=input_bam,
+            extra_label='long_read',
+            job_attrs=self.get_job_attrs(sg),
+        )
+        b.write_output(job.sorted_cram, str(self.expected_outputs(sg)['cram']).removesuffix('.cram'))
+
+        return self.make_outputs(sg, data=self.expected_outputs(sg), jobs=[job])
---
+++ src/lrs_annotation/jobs/BamToCram.py
@@ -0,0 +1,61 @@
+"""
+Convert BAM to CRAM.
+"""
+
+from hailtop.batch import ResourceGroup
+from hailtop.batch.job import Job
+
+from cpg_utils import to_path
+from cpg_utils.config import config_retrieve, image_path, reference_path
+from cpg_utils.hail_batch import Batch, command
+from cpg_flow.resources import STANDARD
+
+
+def bam_to_cram(
+    b: Batch,
+    input_bam: ResourceGroup,
+    job_attrs: dict | None = None,
+) -> tuple[Job, ResourceGroup]:
+    """
+    Convert a BAM file to a CRAM file.
+    """
+    assert isinstance(input_bam, ResourceGroup)
+
+    job_name = 'bam_to_cram'
+
+    convert_tool = 'samtools_view'
+    j_attrs = (job_attrs or {}) | {'label': job_name, 'tool': convert_tool}
+    j = b.new_job(name=job_name, attributes=j_attrs)
+    j.image(image_path('samtools'))
+
+    requested_nthreads = config_retrieve(['resource_overrides', 'bam_to_cram', 'nthreads'], 1)
+    reference_fasta_path = to_path(config_retrieve(['workflow', 'ref_fasta'], reference_path('broad/ref_fasta')))
+
+    # Get fasta file
+    fasta = b.read_input_group(
+        fasta=reference_fasta_path,
+        fasta_fai=f'{reference_fasta_path}.fai',
+    )
+
+    # Set resource requirements
+    nthreads = requested_nthreads or 8
+    res = STANDARD.set_resources(
+        j,
+        ncpu=nthreads,
+        storage_gb=config_retrieve(['resource_overrides', 'bam_to_cram', 'storage_gb'], 50),
+    )
+
+    j.declare_resource_group(
+        sorted_cram={
+            'cram': '{root}.cram',
+            'cram.crai': '{root}.cram.crai',
+        },
+    )
+
+    cmd = f"""
+    samtools view -@ {res.get_nthreads() - 1} -T {fasta.fasta} -C {input_bam.bam} | \
+    tee {j.sorted_cram["cram"]} | samtools index -@ {res.get_nthreads() - 1} - {j.sorted_cram["cram.crai"]}
+    """
+    j.command(command(cmd, monitor_space=True))
+
+    return j, j.sorted_cram
