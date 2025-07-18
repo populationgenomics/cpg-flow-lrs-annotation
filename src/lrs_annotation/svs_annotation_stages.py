5ad41f002dbf7a0fc721e9542d555eb633842c6d
EddieLF <eddieformaini@gmail.com>
2025-07-18 11:56:10 +1000

    Fix sv subset sg ids

--- src/lrs_annotation/svs_annotation_stages.py
+++ src/lrs_annotation/svs_annotation_stages.py
@@ -364,7 +364,7 @@ class SubsetSVsMtToDatasetWithHail(stage.DatasetStage):
 
         jobs = annotate_dataset_jobs_sv(
             mt_path=mt_path,
-            sg_ids=get_sgs_from_datasets([dataset.name]),
+            sg_ids=dataset.get_sequencing_group_ids(),
             out_mt_path=outputs['mt'],
             tmp_prefix=checkpoint_prefix,
             job_attrs=self.get_job_attrs(dataset),
