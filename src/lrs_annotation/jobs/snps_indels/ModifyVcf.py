1168c3b88067eb6d9d40156c92beadabcf1d8634
EddieLF <34049565+EddieLF@users.noreply.github.com>
2025-10-27 19:43:08 +1100

    Snps indels vcf modifier (#13)
    * Script to read GZipped VCFs and uppercase the REF allele strings
    
    * Modify VCF with bcftools view
    
    * Bump version: 0.1.12 → 0.1.13
    
    * Modify VCF with bcftools view and awk
    
    * Add comment about awking
    
    * Bump version: 0.1.13 → 0.1.14
    
    * Remove Path type
    
    * Unbump the extra bump
    
    * Uppercase the ALT allele too
    
    * Fix cmd string
    
    * Pipe straight from sort to awk
    
    * Remove -W=tbi
    
    * Update requirements to use latest cpg-flow version
    
    * workflow name for cpg_flow v1.0.0
    
    * Add -test to dataset names for test runs

---
+++ src/lrs_annotation/jobs/snps_indels/ModifyVcf.py
@@ -0,0 +1,48 @@
+
+from hailtop.batch.job import Job
+
+from cpg_utils.config import config_retrieve
+from cpg_utils.hail_batch import get_batch
+
+from lrs_annotation.utils import get_resource_overrides_for_job
+
+def bcftools_reformat(
+    vcf_path: str,
+    job_name: str,
+    job_attrs: dict,
+    lrs_sg_id_mapping_path: str,
+) -> Job:
+    """
+    Reformat SNPs and Indels VCFs using bcftools.
+
+    Reheader the VCF to replace the LRS IDs with the SG IDs with bcftools reheader.
+    Normalise the VCF with the reference genome and bcftools norm.
+    Uppercases the allele strings with awk.
+    Finally, sort and index the VCF with bcftools sort.
+    """
+    # Input files
+    local_vcf = get_batch().read_input(vcf_path)
+    local_id_mapping = get_batch().read_input(lrs_sg_id_mapping_path)
+
+    # Required file for normalisation
+    ref_fasta = config_retrieve(['workflow', 'ref_fasta'])
+    fasta = get_batch().read_input_group(**{'fa': ref_fasta, 'fa.fai': f'{ref_fasta}.fai'})['fa']
+
+    # Use BCFtools to reheader the VCF, replacing the LRS IDs with the SG IDs
+    job = get_batch().new_job(job_name, job_attrs)
+    job.image(image=config_retrieve(['images', 'bcftools']))
+    job.declare_resource_group(
+        vcf_out={'vcf.gz': '{root}.vcf.gz', 'vcf.gz.tbi': '{root}.vcf.gz.tbi'}
+    )
+
+    job = get_resource_overrides_for_job(job, 'reformat_snps_indels_vcf')
+    job.command(
+        f'bcftools reheader --samples {local_id_mapping} {local_vcf} | '
+        f'bcftools norm -m -any -f {fasta} -c s -Ou | '
+        f'bcftools sort -Oz - | '
+            # Uppercase the allele strings with awk
+            "awk 'BEGIN{OFS=\"\t\"} /^#/ {print; next} {$4=toupper($4); $5=toupper($5); print}' | "
+            f'bgzip > {job.vcf_out["vcf.gz"]} && '
+        f'tabix -p vcf {job.vcf_out["vcf.gz"]}'
+    )
+    return job
