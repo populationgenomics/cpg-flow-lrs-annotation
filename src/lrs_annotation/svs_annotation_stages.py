98780438cf0d8de1eb92f15331a7ef05169f4c4f
EddieLF <eddieformaini@gmail.com>
2025-12-15 12:31:37 +1100

    Subset SV MT and export SV ES-Index only for config specified datasets

--- src/lrs_annotation/svs_annotation_stages.py
+++ src/lrs_annotation/svs_annotation_stages.py
@@ -363,6 +363,12 @@ class SubsetSVsMtToDatasetWithHail(stage.DatasetStage):
             dataset (Dataset): SGIDs specific to this dataset/project
             inputs ():
         """
+        # only create matrixtables for datasets specified in the config
+        # or, if this config option is not set, run for all datasets
+        eligible_datasets = config_retrieve(['workflow', 'write_mt_for_datasets'], default=None)
+        if eligible_datasets is not None and dataset.name not in eligible_datasets:
+            logger.info(f'Skipping MT writing for {dataset}')
+            return self.make_outputs(dataset)
         mt_path = inputs.as_path(target=get_multicohort(), stage=AnnotateCohortSVsMtFromVcfWithHail, key='mt')
         outputs = self.expected_outputs(dataset)
 
@@ -407,7 +413,12 @@ class ExportSVsMtToElasticIndex(stage.DatasetStage):
         """
         Uses the non-DataProc MT-to-ES conversion script
         """
-
+        # only create the elasticsearch index for the datasets specified in the config
+        # or, if this config option is not set, create it for all datasets
+        eligible_datasets = config_retrieve(['workflow', 'create_es_index_for_datasets'], default=None)
+        if eligible_datasets is not None and dataset.name not in eligible_datasets:
+            logger.info(f'Skipping ES index creation for {dataset}')
+            return None
         # try to generate a password here - we'll find out inside the script anyway, but
         # by that point we'd already have localised the MT, wasting time and money
         try:
