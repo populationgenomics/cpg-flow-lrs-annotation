9191ccf0c6f735e38e9d7d42319d32798f71171b
EddieLF <34049565+EddieLF@users.noreply.github.com>
2026-08-03 12:49:38 +1000

    Update bam to cram version (#55)
    * Add sequencing_type and input_vcfs filepath to analysis.meta
    
    * Bump version: 0.3.1 → 0.3.2
    
    * Add input_vcfs filepath to analyses meta
    
    * Bump version: 0.3.2 → 0.3.3
    
    * Add output version 3.0 to cram conversion command
    
    * Bump version: 0.3.3 → 0.3.4
    
    * Update outputs writing

--- src/lrs_annotation/jobs/BamToCram.py
+++ src/lrs_annotation/jobs/BamToCram.py
@@ -14,6 +14,7 @@ from cpg_utils.hail_batch import Batch, command
 def bam_to_cram(
     b: Batch,
     input_bam: ResourceGroup,
+    outputs: dict[str, str],
     job_attrs: dict | None = None,
 ) -> Job:
     """
@@ -39,7 +40,7 @@ def bam_to_cram(
     # Set resource requirements
     res = STANDARD.set_resources(
         j=j,
-        ncpu=config_retrieve(['resource_overrides', 'bam_to_cram', 'cpu_cores'], 1),
+        ncpu=config_retrieve(['workflow', 'resource_overrides', 'bam_to_cram', 'cpu_cores'], 1),
         storage_gb=config_retrieve(['workflow', 'resource_overrides', 'bam_to_cram', 'storage_gib'], 50),
     )
 
@@ -51,9 +52,10 @@ def bam_to_cram(
     )
 
     cmd = f"""
-    samtools view -@ {res.get_nthreads() - 1} -T {fasta.fasta} -C {input_bam.bam} | \
-    tee {j.sorted_cram['cram']} | samtools index -@ {res.get_nthreads() - 1} - {j.sorted_cram['cram.crai']}
+    samtools view -@ {res.get_nthreads() - 1} -T {fasta.fasta} -C {input_bam.bam} \
+    --write-index -O cram,version=3.0 -o {j.sorted_cram.cram}
     """
     j.command(command(cmd, monitor_space=True))
 
+    b.write_output(j.sorted_cram, str(outputs['cram']).removesuffix('.cram'))
     return j
