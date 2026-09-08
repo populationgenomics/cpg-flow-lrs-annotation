c86e4dfd83b1657906d3ff39d3597de427b9dc4f
EddieLF <34049565+EddieLF@users.noreply.github.com>
2026-09-09 09:43:43 +1000

    Fix analysis update arg, move cram index and somalier (#61)
    * Fix analysis update arg
    
    * Also move .crai and .somalier
    
    * Linting

--- src/lrs_annotation/scripts/fix_cram_paths.py
+++ src/lrs_annotation/scripts/fix_cram_paths.py
@@ -47,17 +47,28 @@ def fix_cram_paths(result, dry_run=False):
             source_blob = bucket.blob(source_path.blob)
             new_blob_str = source_path.blob.replace('long_read/long_read/cram', 'long_read/cram')
 
+            crai_blob = bucket.blob(source_path.blob.replace('.cram', '.cram.crai'))
+            new_crai_str = new_blob_str.replace('.cram', '.cram.crai')
+            somalier_blob = bucket.blob(source_path.blob.replace('.cram', '.cram.somalier'))
+            new_somalier_str = new_blob_str.replace('.cram', '.cram.somalier')
+
             if not dry_run:
                 loguru.logger.info(f'Moving {source_blob.name} to {new_blob_str} in bucket {bucket.name}')
                 bucket.copy_blob(source_blob, bucket, new_blob_str)
+                bucket.copy_blob(crai_blob, bucket, new_crai_str)
+                bucket.copy_blob(somalier_blob, bucket, new_somalier_str)
                 loguru.logger.info(f'Copied. Deleting original: {source_path.blob} from bucket {bucket.name}')
                 bucket.delete_blob(source_path.blob)
+                bucket.delete_blob(crai_blob.name)
+                bucket.delete_blob(somalier_blob.name)
                 loguru.logger.info(
                     f'Updating analysis record for {analysis["id"]} with new path gs://{bucket.name}/{new_blob_str}'
                 )
                 AnalysisApi().update_analysis(
                     analysis_id=analysis['id'],
-                    analysis_update=AnalysisUpdateModel(outputs={'basename': f'gs://{bucket.name}/{new_blob_str}'}),
+                    analysis_update_model=AnalysisUpdateModel(
+                        outputs={'basename': f'gs://{bucket.name}/{new_blob_str}'}
+                    ),
                 )
             else:
                 loguru.logger.info(f'Dry run: would move {source_blob.name} to {new_blob_str} in {bucket.name}')
